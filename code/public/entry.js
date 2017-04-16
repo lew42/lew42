164a3d7518bc6c28eb040369b17b3c33e70e6018
Michael Lewis <mike@lew42.com>
2017-04-16 07:06:03 -0500

    yea

--- code/public/entry.js
+++ code/public/entry.js
@@ -3,16 +3,6 @@ var Promise = require("promise-polyfill");
 if (!window.Promise){
 	window.Promise = Promise;
 }
-var Logger = require("log42/v1");
-var log = window.log = Logger({
-	adopt: false,
-	globalLogger: true,
-});
-log.becomeCaptor();
-
-
-
-log.group("entry.js", function(){
 
 var $ = window.$ = require("jquery");
 
@@ -23,13 +13,11 @@ require("../css/styles.less");
 
 var App2, View, homepage, Header, Footer; 
 
-log.groupc("requires", function(){
-	App2 = require("app42/App2");
-	View = require("view42/v8");
-	homepage = require("./homepage");
-	Header = require("./header.js");
-	Footer = require("./footer.js");
-});
+App2 = require("app42/App2");
+View = require("view42/v8");
+homepage = require("./homepage");
+Header = require("./header.js");
+Footer = require("./footer.js");
 
 var app = App2({
 	name: "app",
@@ -49,40 +37,51 @@ var app = App2({
 
 		Footer();
 
-		// this.adminPanel = View(function(){
-		// 	View({
-		// 		content: function(){
-		// 			this.btn = View("close");
-		// 			this.icon = View("full");
-		// 		},
-		// 		behaviors: function(){
-		// 			this.btn.click(function(){
-		// 				app.adminPanel.hide();
-		// 			});
-		// 			this.icon.click(function(){
-		// 				app.adminPanel.toggleClass("fullscreen");
-		// 			});
-		// 		}
-		// 	});
-		// 	this.contents = View(function(){
-		// 		app.log.logger.appLogger = true;
-		// 		app.log.logger.label = "app-logger";
-		// 		log.expand = true;
-		// 		log.render(true);
-		// 	}).addClass("contents");
-		// }).addClass("admin-panel").appendTo("body");
-		this.adminPanel = View();
+		this.adminPanel = View(function(){
+			this.tabs = View({
+				content: function(){
+					this.btn = View("close");
+					this.icon = View("full");
+					this.browse = View("browse");
+					this.log = View("log");
+				},
+				behaviors: function(){
+					this.btn.click(function(){
+						app.adminPanel.hide();
+					});
+					this.icon.click(function(){
+						app.adminPanel.toggleClass("fullscreen");
+					});
+
+					this.browse.click(function(){
+						app.adminPanel.contents.hideAll();
+						app.adminPanel.contents.browse.show();
+					});
+					this.log.click(function(){
+						app.adminPanel.contents.hideAll();
+						app.adminPanel.contents.log.show();
+					});
+				}
+			}).addClass("tabbies");
+			this.contents = View({
+				content: function(){
+					this.browse = View();
+					this.log = View();
+				},
+				hideAll: function(){
+					this.browse.hide();
+					this.log.hide();
+				}
+			}).addClass("contents");
+
+			this.contents.hideAll();
+			this.contents.browse.show();
+		}).addClass("admin-panel").appendTo("body");
 	}
 });
 
 var track = require("track42");
 track.app = app;
 
-// app.log.logger.becomeCaptor();
-
-
-// $(function(){
-// 	app.log("document.ready");
-// });
-
-});
+var Logger = require("log42/v4");
+Logger.prototype.app = app;
